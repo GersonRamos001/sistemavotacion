870000c7ec10cd5e8131031591b740e68d2ba3e1
Gerson Vladimir Ramos <ramosargueta@gmail.com>
2022-09-30 18:20:10 -0600

    Inicio

---
+++ src/main/java/gob/sv/sistemavotacion/SistemavotacionApplication.java
@@ -0,0 +1,13 @@
+package gob.sv.sistemavotacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavotacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemavotacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gob/sv/sistemavotacion/SistemavotacionApplicationTests.java
@@ -0,0 +1,13 @@
+package gob.sv.sistemavotacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemavotacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
